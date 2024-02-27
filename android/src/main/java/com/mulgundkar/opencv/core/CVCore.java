55eb7c762f68825c948fe9596597a2b6f7f6dbb2
Altynbek <a.shaymuhanov@strongte.am>
2024-02-27 16:08:45 +0600

    +500 to ytranslation

--- android/src/main/java/com/mulgundkar/opencv/core/CVCore.java
+++ android/src/main/java/com/mulgundkar/opencv/core/CVCore.java
@@ -788,15 +788,15 @@ public class CVCore {
             int transformedWidth = (int) Math.round(boundingBox[1].x - boundingBox[0].x + 0.5);
             int transformedHeight = (int) Math.round(boundingBox[1].y - boundingBox[0].y + 0.5);
             int xTranslation = -(int) Math.round(boundingBox[0].x);
-            int yTranslation = -(int) Math.round(boundingBox[1].y);
-            System.out.println("yTranslation2: " + yTranslation);
+            int yTranslation = -(int) Math.round(boundingBox[0].y + 500);
+            System.out.println("yT: " + yTranslation);
 
 
             Mat finalTranslationMatrix = Mat.eye(3, 3, CV_32F);
             double[] row0 = translationMatrix.get(0, 2);
             double[] row1 = translationMatrix.get(1, 2);
             row0[0] += xTranslation;
-            row1[0] -= 0;
+            row1[0] += yTranslation;
             finalTranslationMatrix.put(0, 2, row0);
             finalTranslationMatrix.put(1, 2, row1);
             Mat combinedTransform = new Mat();
