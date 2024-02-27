c0764f217fa4b99cb867d5bd9c33d1fb862ccf1e
Altynbek <a.shaymuhanov@strongte.am>
2024-02-27 17:28:58 +0600

    print some info

--- android/src/main/java/com/mulgundkar/opencv/core/CVCore.java
+++ android/src/main/java/com/mulgundkar/opencv/core/CVCore.java
@@ -788,13 +788,16 @@ public class CVCore {
             int transformedWidth = (int) Math.round(boundingBox[1].x - boundingBox[0].x + 0.5);
             int transformedHeight = (int) Math.round(boundingBox[1].y - boundingBox[0].y + 0.5);
             int xTranslation = -(int) Math.round(boundingBox[0].x);
-            int yTranslation = -(int) Math.round(boundingBox[0].y + 500);
-            System.out.println("yT: " + yTranslation);
-
+            int yTranslation = -(int) Math.round(boundingBox[0].y);
+            System.out.println("boundingBox[1].y: " + boundingBox[1].y);
+            System.out.println("boundingBox[0].y: " + boundingBox[0].y);
+            System.out.println("tranheight: " + transformedHeight);
 
             Mat finalTranslationMatrix = Mat.eye(3, 3, CV_32F);
             double[] row0 = translationMatrix.get(0, 2);
             double[] row1 = translationMatrix.get(1, 2);
+            System.out.println("row1: " + row1);
+            System.out.println("row0: " + row0);
             row0[0] += xTranslation;
             row1[0] += yTranslation;
             finalTranslationMatrix.put(0, 2, row0);
