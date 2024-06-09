805a6070a0a14d17933001b3bb511a3447b4eae0
Altynbek <a.shaymuhanov@strongte.am>
2024-06-09 19:23:42 +0500

    -380 without x

--- android/src/main/java/com/mulgundkar/opencv/core/CVCore.java
+++ android/src/main/java/com/mulgundkar/opencv/core/CVCore.java
@@ -787,7 +787,7 @@ public class CVCore {
             Point[] boundingBox = computeAxisAlignedBoundingBox(transformedRect);
             int transformedWidth = (int) Math.round(boundingBox[1].x - boundingBox[0].x + 0.5);
             int transformedHeight = (int) Math.round(boundingBox[1].y - boundingBox[0].y + 0.5);
-            int xTranslation = -(int) Math.round(-350);
+            int xTranslation = -(int) Math.round(-380);
 //            int yTranslation = -(int) Math.round(boundingBox[0].y);
             int yTranslation = -(int) Math.round(translationMatrix.get(1, 2)[0] + boundingBox[0].y);
             System.out.println("boundingBox[1].x: " + boundingBox[1].x);
@@ -795,6 +795,7 @@ public class CVCore {
             System.out.println("translationMatrix.get(1, 2)[0]: " + translationMatrix.get(1, 2)[0]);
             System.out.println("translationMatrix.get(0, 2)[0]: " + translationMatrix.get(0, 2)[0]);
             System.out.println("transformedHeight: " + transformedHeight);
+            System.out.println("transformedWidth: " + transformedWidth);
 
             Mat finalTranslationMatrix = Mat.eye(3, 3, CV_32F);
             double[] row0 = translationMatrix.get(0, 2);
